2a97cde4eae79c7419fb2f2f6a66790a2eb114fa
GustavFL2000 <gustavfl13@gmail.com>
2026-05-05 11:17:14 +0200

    link mellem påfyldning og destillering

--- src/model/Destillering.java
+++ src/model/Destillering.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Destillering {
     int destilleringsID;
@@ -12,6 +14,9 @@ public class Destillering {
     String kommentar;
     Maltbatch maltbatch;
 
+    //Composition --> 0..* Påfyldning
+    private List<Påfyldning> påfyldninger = new ArrayList<>();
+
     public Destillering(int destilleringsID, LocalDate startDato, LocalDate slutDato, double alkoholProcent, boolean rygematriale, String kommentar, double væskeMængde, Maltbatch maltbatch) {
         this.destilleringsID = destilleringsID;
         this.startDato = startDato;
@@ -21,6 +26,16 @@ public class Destillering {
         this.kommentar = kommentar;
         this.væskeMængde = væskeMængde;
         this.maltbatch = maltbatch;
+    }
+
+    public List<Påfyldning> getPåfyldninger() {
+        return new ArrayList<>(påfyldninger);
+    }
+
+    public Påfyldning createPåfyldning(LocalDate dato, double mængde, double alkoholProcent, Destillering destillering){
+        Påfyldning påfyldning = new Påfyldning(dato,mængde,alkoholProcent,this);
+        påfyldninger.add(påfyldning);
+        return påfyldning;
     }
 
     public int getDestilleringsID() {
--- src/model/Påfyldning.java
+++ src/model/Påfyldning.java
@@ -6,4 +6,17 @@ public class Påfyldning {
     LocalDate dato;
     double mængde;
     double alkoholProcent;
+    private Destillering destillering;
+
+    public Påfyldning(LocalDate dato, double mængde, double alkoholProcent, Destillering destillering) {
+        this.dato = dato;
+        this.mængde = mængde;
+        this.alkoholProcent = alkoholProcent;
+        this.destillering = destillering;
+    }
+
+    public Destillering getDestillering(){
+        return destillering;
+    }
+
 }
