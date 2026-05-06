8296437e199637c4f7517350e0ab3393170c6759
Gustav Færmann Lassen <gustavfl13@gmail.com>
2026-05-06 09:51:16 +0200

    javafx start

---
+++ src/gui/Gui.java
@@ -0,0 +1,28 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Gui extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Gui Demo 1");
+        GridPane pane = new GridPane();
+        this.initContent(pane);
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void initContent(GridPane pane) {
+        pane.setGridLinesVisible(true);
+        pane.setPadding(new Insets(20));
+        // set horizontal gap between components
+        pane.setHgap(10);
+        // set vertical gap between components
+        pane.setVgap(10);
+    }
+}
