f6b0da911e4860a70363a7288faa3292de582e99
Gustav Færmann Lassen <gustavfl13@gmail.com>
2026-05-13 10:17:18 +0200

    controller gui

--- src/gui/App.java
+++ src/gui/App.java
@@ -1,9 +1,19 @@
 package gui;
 
+import controller.Controller;
 import javafx.application.Application;
+import storage.IStorage;
+import storage.Storage;
 
 public class App {
+
     public static void main(String[] args) {
+
+        IStorage storage = new Storage();
+        Controller controller = new Controller(storage);
+
+        Gui.setController(controller);
+
         Application.launch(Gui.class);
     }
 }
