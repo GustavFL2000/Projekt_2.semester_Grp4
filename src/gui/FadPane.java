642086fa8e325348f90155902a003c368bbe1334
Gustav Færmann Lassen <gustavfl13@gmail.com>
2026-05-14 20:55:09 +0200

    søg fad i gui

--- src/gui/FadPane.java
+++ src/gui/FadPane.java
@@ -7,106 +7,110 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import model.Destillat;
 import model.Fad;
+import model.Lager;
 import model.Leverandør;
-import java.time.LocalDate;
 
+import java.time.LocalDate;
+import java.util.List;
 
 public class FadPane extends GridPane {
+
     private Controller controller;
     private ListView<Fad> lsvFade;
     private ComboBox<Fad> cobFad;
     private ComboBox<Destillat> cobDestillat;
 
     public FadPane(Controller controller) {
-        this.controller=controller;
+        this.controller = controller;
 
         this.setPadding(new Insets(20));
         this.setHgap(10);
         this.setVgap(10);
 
-        //Fade
+        // Fade
         Label lblTilgængeligeFade = new Label("Tilgængelige fade");
-        this.add(lblTilgængeligeFade,2,0);
+        this.add(lblTilgængeligeFade, 2, 0);
+
         lsvFade = new ListView<>();
         lsvFade.setItems(FXCollections.observableArrayList(controller.getFade()));
-        this.add(lsvFade, 2, 1, 1, 10);
+        this.add(lsvFade, 2, 1, 1, 15);
 
         opretFad();
         fordelDestillatPåFad();
-
+        søgFade();
     }
 
-    public void opretFad (){
+    public void opretFad() {
+
         Label lbl = new Label("Opret fad");
         this.add(lbl, 0, 0);
 
-        //(fadNr, land, størrelse, tidligereIndhold, leverandør);
-
         // land
         Label lblLand = new Label("Indtast et land");
-        this.add(lblLand,0, 1);
+        this.add(lblLand, 0, 1);
+
         TextField txtLand = new TextField();
-        this.add(txtLand, 0,2);
+        this.add(txtLand, 0, 2);
 
-        //størrelse
+        // størrelse
         Label lblStørrelse = new Label("Indtast størrelse på fad");
-        this.add(lblStørrelse,0, 3);
-        TextField txtStørrelse = new TextField();
-        this.add(txtStørrelse, 0,4);
+        this.add(lblStørrelse, 0, 3);
 
-        //tidligereIndhold
+        TextField txtStørrelse = new TextField();
+        this.add(txtStørrelse, 0, 4);
+
+        // tidligereIndhold
         Label lblTidligereIndhold = new Label("Indtast tidligere indhold");
         this.add(lblTidligereIndhold, 0, 5);
+
         TextField txtTidligereIndhold = new TextField();
         this.add(txtTidligereIndhold, 0, 6);
 
-        //Leverandør
+        // Leverandør
         Label lblLeverandør = new Label("Leverandør");
         this.add(lblLeverandør, 0, 7);
+
         ComboBox<Leverandør> cobLeverandør = new ComboBox<>();
         cobLeverandør.setItems(FXCollections.observableArrayList(controller.getLeverandører()));
         this.add(cobLeverandør, 0, 8);
 
-
-        //Opret knap
+        // Opret knap
         Button btnOpretKnap = new Button("Opret fad");
-        this.add(btnOpretKnap,0, 9);
-        btnOpretKnap.setOnAction(event ->{
+        this.add(btnOpretKnap, 0, 9);
+
+        btnOpretKnap.setOnAction(event -> {
+
             try {
+
                 String land = txtLand.getText();
                 double størrelse = Double.parseDouble(txtStørrelse.getText());
                 String tidligereIndhold = txtTidligereIndhold.getText();
                 Leverandør leverandør = cobLeverandør.getSelectionModel().getSelectedItem();
+
                 controller.createFad(land, størrelse, tidligereIndhold, leverandør);
 
-                //Opdaterer listView efter at man har trykket på knappen
                 updateControls();
 
-                // fjerner alt tekst efter oprettelse
                 txtLand.clear();
                 txtStørrelse.clear();
                 txtTidligereIndhold.clear();
                 cobLeverandør.getSelectionModel().clearSelection();
 
-                //Bekræftelse alert
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
-
                 alert.setTitle("Succes");
                 alert.setHeaderText("Fad oprettet");
                 alert.setContentText("Fad blev oprettet korrekt.");
-
                 alert.showAndWait();
 
             } catch (NumberFormatException e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
 
+                Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Fejl");
                 alert.setHeaderText("Ugyldigt tal");
                 alert.setContentText("Du kan kun indtaste tal i størrelse");
-
                 alert.showAndWait();
 
-            }catch (IllegalArgumentException e) {
+            } catch (IllegalArgumentException e) {
 
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Fejl");
@@ -117,9 +121,103 @@ public class FadPane extends GridPane {
         });
     }
 
+    public void søgFade() {
 
-    //Opdaterer comboBoxne så der kommer destilleringer og destillater ind
+        Label lblSøg = new Label("Søg efter fade");
+        this.add(lblSøg, 1, 0);
+
+        // fadNr
+        Label lblFadNr = new Label("Fad nr");
+        this.add(lblFadNr, 1, 1);
+
+        TextField txtFadNr = new TextField();
+        this.add(txtFadNr, 1, 2);
+
+        // lager
+        Label lblLager = new Label("Lager");
+        this.add(lblLager, 1, 3);
+
+        ComboBox<Lager> cobLager = new ComboBox<>();
+        cobLager.setItems(FXCollections.observableArrayList(controller.getLager()));
+        cobLager.getItems().addFirst(null);
+        this.add(cobLager, 1, 4);
+
+        // tidligere indhold
+        Label lblTidligereIndhold = new Label("Tidligere indhold");
+        this.add(lblTidligereIndhold, 1, 5);
+
+        TextField txtTidligereIndhold = new TextField();
+        this.add(txtTidligereIndhold, 1, 6);
+
+        // alder
+        Label lblAlder = new Label("Alder");
+        this.add(lblAlder, 1, 7);
+
+        TextField txtAlder = new TextField();
+        this.add(txtAlder, 1, 8);
+
+        // søg knap
+        Button btnSøg = new Button("Søg");
+        this.add(btnSøg, 1, 9);
+
+        btnSøg.setOnAction(event -> {
+
+            try {
+
+                Integer fadNr = null;
+                Lager lager = null;
+                String tidligereIndhold = null;
+                Integer alder = null;
+
+                // fadNr
+                if (!txtFadNr.getText().isBlank()) {
+                    fadNr = Integer.parseInt(txtFadNr.getText());
+                }
+
+                // lager
+                lager = cobLager.getSelectionModel().getSelectedItem();
+
+                // tidligere indhold
+                if (!txtTidligereIndhold.getText().isBlank()) {
+                    tidligereIndhold = txtTidligereIndhold.getText();
+                }
+
+                // alder
+                if (!txtAlder.getText().isBlank()) {
+                    alder = Integer.parseInt(txtAlder.getText());
+                }
+
+                List<Fad> fundneFade = controller.søgEfterFade(
+                        fadNr,
+                        lager,
+                        tidligereIndhold,
+                        alder
+                );
+
+                lsvFade.setItems(FXCollections.observableArrayList(fundneFade));
+
+            } catch (NumberFormatException e) {
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Fejl");
+                alert.setHeaderText("Ugyldigt tal");
+                alert.setContentText("Fad nr og alder skal være tal.");
+                alert.showAndWait();
+
+            } catch (IllegalArgumentException e) {
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Fejl");
+                alert.setHeaderText("Fejl");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+            }
+        });
+    }
+
+    // Opdaterer comboBoxene
     public void updateControls() {
+
         lsvFade.setItems(
                 FXCollections.observableArrayList(controller.getFade())
         );
@@ -131,44 +229,51 @@ public class FadPane extends GridPane {
         cobFad.setItems(
                 FXCollections.observableArrayList(controller.getFade())
         );
-
     }
 
-    public void fordelDestillatPåFad(){
+    public void fordelDestillatPåFad() {
+
         Label lbl = new Label("Fordel destillat på fad");
         this.add(lbl, 3, 0);
 
-        //Destillat
+        // Destillat
         Label lblDestillat = new Label("Vælg destillat");
         this.add(lblDestillat, 3, 1);
+
         cobDestillat = new ComboBox<>();
         cobDestillat.setItems(FXCollections.observableArrayList(controller.getDestillater()));
         this.add(cobDestillat, 3, 2);
 
-        //Fad
+        // Fad
         Label lblFad = new Label("Vælg fad");
         this.add(lblFad, 3, 3);
+
         cobFad = new ComboBox<>();
         cobFad.setItems(FXCollections.observableArrayList(controller.getFade()));
         this.add(cobFad, 3, 4);
 
-        //Mængde
+        // Mængde
         Label lblMængde = new Label("Indtast mængde");
         this.add(lblMængde, 3, 5);
+
         TextField txtMængde = new TextField();
         this.add(txtMængde, 3, 6);
 
-        // StartDato
+        // Dato
         Label lblDato = new Label("Vælg dato");
-        this.add(lblDato,3, 7);
-        DatePicker dpDato = new DatePicker();
-        this.add(dpDato, 3,8);
+        this.add(lblDato, 3, 7);
 
-        //Knap til whiskysammensætning
+        DatePicker dpDato = new DatePicker();
+        this.add(dpDato, 3, 8);
+
+        // Knap
         Button btnFordel = new Button("Tilføj mængde til fad");
         this.add(btnFordel, 3, 9);
+
         btnFordel.setOnAction(event -> {
+
             try {
+
                 Destillat destillat = cobDestillat.getSelectionModel().getSelectedItem();
                 Fad fad = cobFad.getSelectionModel().getSelectedItem();
                 double mængde = Double.parseDouble(txtMængde.getText());
@@ -176,7 +281,6 @@ public class FadPane extends GridPane {
 
                 controller.createPåfyldning(mængde, dato, destillat, fad);
 
-                //Opdaterer comboBoksne efter at man har trykket på knappen
                 updateControls();
 
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -186,6 +290,7 @@ public class FadPane extends GridPane {
                 alert.showAndWait();
 
             } catch (NumberFormatException e) {
+
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Fejl");
                 alert.setHeaderText("Ugyldigt tal");
@@ -193,6 +298,7 @@ public class FadPane extends GridPane {
                 alert.showAndWait();
 
             } catch (IllegalArgumentException e) {
+
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Fejl");
                 alert.setHeaderText("Ugyldige data");
@@ -201,6 +307,4 @@ public class FadPane extends GridPane {
             }
         });
     }
-
-
 }
