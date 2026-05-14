c0d45d60e6cd883b8554a8c766c43e2b9ce07ec1
Gustav Færmann Lassen <gustavfl13@gmail.com>
2026-05-14 14:18:17 +0200

    gui

--- src/gui/LagerPane.java
+++ src/gui/LagerPane.java
@@ -6,11 +6,14 @@ import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import model.Fad;
-import model.Leverandør;
+import model.Lager;
+import model.Reol;
 
 public class LagerPane extends GridPane {
+
     private Controller controller;
-    private ListView lagerlist;
+    private ListView<Lager> lagerlist;
+    private ListView<Reol> reolList;
     private ComboBox<Fad> cobFad;
 
     public LagerPane(Controller controller) {
@@ -23,56 +26,106 @@ public class LagerPane extends GridPane {
         opretLager();
         placerFadpåLager();
 
+        // Liste med lagre
         lagerlist = new ListView<>();
-        lagerlist.setItems(FXCollections.observableArrayList(controller.getLager()));
+        lagerlist.setItems(
+                FXCollections.observableArrayList(controller.getLager())
+        );
+
         this.add(lagerlist, 2, 1, 1, 10);
+
+        // Når man vælger et lager
+        lagerlist.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldLager, selectedLager) -> {
+
+                    if (selectedLager != null) {
+
+                        // Vis reoler fra valgt lager
+                        reolList.setItems(
+                                FXCollections.observableArrayList(
+                                        selectedLager.getReoler()
+                                )
+                        );
+                    }
+                });
     }
 
-    public void placerFadpåLager(){
+    public void placerFadpåLager() {
+
+        // Vælg fad
         Label lblFad = new Label("Vælg fad");
-        this.add(lblFad, 3, 0);
+        this.add(lblFad, 4, 0);
+
         cobFad = new ComboBox<>();
-        cobFad.setItems(FXCollections.observableArrayList(controller.getFade()));
-        this.add(cobFad, 3, 1);
+        cobFad.setItems(
+                FXCollections.observableArrayList(controller.getFade())
+        );
+
+        this.add(cobFad, 4, 1);
+
+        // Vælg reol fra lager
+
+        reolList = new ListView<>();
+        this.add(reolList, 3, 1, 1, 10);
+
+        // Hvis man vælger en reol
+        reolList.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldReol, selectedReol) -> {
+
+                    if (selectedReol != null) {
+                        System.out.println("Valgt reol: " + selectedReol);
+                    }
+                });
     }
 
+    public void opretLager() {
 
-    public void opretLager (){
-        //String lagerNavn, String adresse
+        // String lagerNavn, String adresse
         Label lbl = new Label("Opret lager");
         this.add(lbl, 0, 0);
 
-        //lagerNavn
+        // lagerNavn
         Label lblLagerNavn = new Label("Indtast lagernavn");
         this.add(lblLagerNavn, 0, 1);
+
         TextField txtLagerNavn = new TextField();
         this.add(txtLagerNavn, 0, 2);
 
-        //adresse
+        // adresse
         Label lblAdresse = new Label("Indtast adresse");
         this.add(lblAdresse, 0, 3);
+
         TextField txtAdresse = new TextField();
         this.add(txtAdresse, 0, 4);
 
-        //antal reoler
+        // antal reoler
         Label lblAntalReoler = new Label("Indtast antal reoler");
         this.add(lblAntalReoler, 0, 5);
 
         TextField txtAntalReoler = new TextField();
         this.add(txtAntalReoler, 0, 6);
 
-        //Knap til opretlager
+        // Knap til opretlager
         Button btnFordel = new Button("Opret lager");
         this.add(btnFordel, 0, 7);
+
         btnFordel.setOnAction(event -> {
+
             try {
+
                 String lagerNavn = txtLagerNavn.getText();
                 String adresse = txtAdresse.getText();
-                int antalReoler = Integer.parseInt(txtAntalReoler.getText());
+                int antalReoler = Integer.parseInt(
+                        txtAntalReoler.getText()
+                );
 
-                controller.createLagerMedReoler(lagerNavn, adresse, antalReoler);
+                controller.createLagerMedReoler(
+                        lagerNavn,
+                        adresse,
+                        antalReoler
+                );
 
-                //Opdaterer listView efter man har trykket på opret knappen
+                // Opdaterer ListView
                 updateControls();
 
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -81,12 +134,13 @@ public class LagerPane extends GridPane {
                 alert.setContentText("Lager er nu oprettet");
                 alert.showAndWait();
 
-                //Fjerner tekst efter oprettelse
+                // Ryd felter
                 txtLagerNavn.clear();
                 txtAdresse.clear();
                 txtAntalReoler.clear();
 
             } catch (IllegalArgumentException e) {
+
                 Alert alert = new Alert(Alert.AlertType.ERROR);
                 alert.setTitle("Fejl");
                 alert.setHeaderText("Ugyldige data");
@@ -97,6 +151,7 @@ public class LagerPane extends GridPane {
     }
 
     public void updateControls() {
+
         lagerlist.setItems(
                 FXCollections.observableArrayList(controller.getLager())
         );
